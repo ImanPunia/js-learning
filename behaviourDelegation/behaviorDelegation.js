5a6f6468cfcb6238cf58d520a01336d00ce8ed46
Imandeep Kaur <imanpunia19@gmail.com>
2021-06-15 17:50:30 +1000

    behaviour Delegation example

---
+++ behaviourDelegation/behaviorDelegation.js
@@ -0,0 +1,26 @@
+Vehicle = {
+    engine: 1,
+    ignition: function(){
+        console.log("ignition");
+    } ,
+
+    drive: function(){
+        this.ignition();
+        console.log("drive");
+    }
+}
+
+const Car = Object.create(Vehicle);
+
+Car.setWheel = function(wheels){
+    this.wheels = wheels;
+}
+
+Car.driveCar = function(){
+    this.setWheel(4);
+    this.drive();
+    console.log('rolling on 4 wheels' + this.wheels);
+}
+
+Car.driveCar(); // ignition drive rolling on 4 wheel4 
+Car.drive(); // ignition drive
